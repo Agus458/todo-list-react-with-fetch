829763a7b444a913985ea0f77b307f5ca725a4af
Agus458 <aguspe458@outlook.com>
2021-05-07 13:29:04 +0000

    todo list con fetch

--- src/js/component/TodoFooter.js
+++ src/js/component/TodoFooter.js
@@ -4,17 +4,31 @@ import PropTypes from "prop-types";
 export default function TodoFooter(props) {
 	return (
 		<div className="card-body">
-			<p>
-				{props.cantTasks === 0
-					? "You are up to date"
-					: props.cantTasks === 1
-					? `${props.cantTasks} task remaining`
-					: `${props.cantTasks} tasks remaining`}
-			</p>
+			<div className="row align-items-center">
+				<div className="col">
+					<p>
+						{props.cantTasks === 0
+							? "You are up to date"
+							: props.cantTasks === 1
+							? `${props.cantTasks} task remaining`
+							: `${props.cantTasks} tasks remaining`}
+					</p>
+				</div>
+				<div className="col d-flex justify-content-end">
+					<button
+						onClick={() => {
+							props.clearAll();
+						}}
+						className="btn btn-danger">
+						Clear All
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
 
 TodoFooter.propTypes = {
-	cantTasks: PropTypes.number
+	cantTasks: PropTypes.number,
+	clearAll: PropTypes.func
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,30 +5,120 @@ import TodoFooter from "./TodoFooter";
 
 export function Home() {
 	const [tasksList, setTasksList] = useState([]);
-	const [cantTasks, setCantTasks] = useState(0);
+	const [user, setUser] = useState(false);
+
+	useEffect(() => {
+		getTasks();
+	}, []);
 
 	function addTask(task) {
 		let newTask = { label: task, done: false };
-		setTasksList([...tasksList, newTask]);
-		setCantTasks(cantTasks + 1);
+		let newTaskList = [...tasksList, newTask];
+		setTasksList(newTaskList);
+		if (user) {
+			updateTasks(newTaskList);
+		} else {
+			createUser();
+			setTimeout(() => {
+				updateTasks(newTaskList);
+			}, 2000);
+		}
 	}
 
 	function removeTask(pos) {
-		setTasksList(
-			tasksList.filter((task, index) => {
-				return index != pos;
-			})
-		);
-		setCantTasks(cantTasks - 1);
+		let newTaskList = tasksList.filter((task, index) => {
+			return index != pos;
+		});
+		setTasksList(newTaskList);
+
+		if (newTaskList.length > 0) {
+			updateTasks(newTaskList);
+		} else {
+			deleteUser();
+		}
+	}
+
+	async function getTasks() {
+		try {
+			let res = await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/Agus458"
+			);
+			let data = await res.json();
+
+			if (!Array.isArray(data)) {
+				await createUser();
+				await getTasks();
+			} else {
+				setTasksList(data);
+				setUser(true);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async function updateTasks(tasks) {
+		try {
+			let options = {
+				method: "PUT",
+				body: JSON.stringify(tasks),
+				headers: { "Content-Type": "application/json" }
+			};
+			await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/Agus458",
+				options
+			);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async function createUser() {
+		try {
+			let options = {
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: { "Content-Type": "application/json" }
+			};
+			await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/Agus458",
+				options
+			);
+			setUser(true);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async function deleteUser() {
+		try {
+			let options = {
+				method: "DELETE"
+			};
+			await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/Agus458",
+				options
+			);
+			setUser(false);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	function clearAll() {
+		if (user) {
+			setTasksList([]);
+			deleteUser();
+		}
 	}
 
 	return (
 		<div className="container text-center">
 			<h1>todos</h1>
 			<div className="card brown shadow-lg">
-				<TaskInput cantTasks={cantTasks} addTask={addTask} />
+				<TaskInput cantTasks={tasksList.length} addTask={addTask} />
 				<TasksList tasksList={tasksList} removeFromList={removeTask} />
-				<TodoFooter cantTasks={cantTasks} />
+				<TodoFooter cantTasks={tasksList.length} clearAll={clearAll} />
 			</div>
 		</div>
 	);
